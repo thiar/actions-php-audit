533c8d19ad4d9fe26b8a7f8ffa56eef156d59188
Thiar Hasbiya <thiar.hasbiya@gmail.com>
2023-06-23 09:43:06 +0700

    add composer package name to the report

--- script/message-composer-audit.php
+++ script/message-composer-audit.php
@@ -3,5 +3,5 @@ $cveData = json_decode($argv[1],true);
 echo('# Composer Audit Report\n=============================\n\n');
 foreach ($cveData['advisories'] as $key => $value) {
     $value = array_values($value);
-    echo('- [ ] ['.$value[0]['cve'].']('.$value[0]['link'].') => '.$value[0]['title'].'\n');
+    echo('- [ ] [['.$value[0]['cve'].']('.$value[0]['link'].')] '.$value[0]['packageName'].' => '.$value[0]['title'].'\n');
 }
