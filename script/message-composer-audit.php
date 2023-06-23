f730ba4cc891ab6784c1022e45eb1d193e5bac81
Thiar Hasbiya <thiar.hasbiya@gmail.com>
2023-06-23 09:39:00 +0700

    reorder array data from composer audit

--- script/message-composer-audit.php
+++ script/message-composer-audit.php
@@ -2,5 +2,6 @@
 $cveData = json_decode($argv[1],true);
 echo('# Composer Audit Report\n=============================\n\n');
 foreach ($cveData['advisories'] as $key => $value) {
+    $value = array_values($value);
     echo('- [ ] ['.$value[0]['cve'].']('.$value[0]['link'].') => '.$value[0]['title'].'\n');
 }
